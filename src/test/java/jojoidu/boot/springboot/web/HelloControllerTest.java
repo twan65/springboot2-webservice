7949d87cebc6855ffe196fcecc8da939b56e0db4
twan65 <cx.twan65@gmail.com>
2020-08-09 17:36:25 +0900

    API JSONテストコード実装

--- src/test/java/jojoidu/boot/springboot/web/HelloControllerTest.java
+++ src/test/java/jojoidu/boot/springboot/web/HelloControllerTest.java
@@ -7,9 +7,9 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
 @RunWith(SpringRunner.class)
@@ -26,6 +26,20 @@ public class HelloControllerTest {
         mvc.perform(get("/hello"))
                 .andExpect(status().isOk())
                 .andExpect(content().string(hello));
+    }
+
+    @Test
+    public void returnHelloDto() throws Exception {
+        String name = "test";
+        int amount = 1000;
+
+        mvc.perform(
+                get("/hello/dto")
+                .param("name", name) // APIテストのリクエストパラメータを設定。Stringのみ許可
+                .param("amount", String.valueOf(amount))
+        ).andExpect(status().isOk())
+        .andExpect(jsonPath("$.name", is(name))) // jsonpath：JSONレスポンスをフィールドごとに検証できる
+        .andExpect(jsonPath("$.amount", is(amount))); // $を基準でフィールドを記述
     }
 
 }
