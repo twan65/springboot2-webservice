cff2dd68ebd12e847150921d9359b9968407619f
twan65 <cx.twan65@gmail.com>
2020-08-17 18:52:13 +0900

    Spring Security & Google OAuth2適用

--- src/main/java/jojoidu/boot/springboot/web/IndexController.java
+++ src/main/java/jojoidu/boot/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package jojoidu.boot.springboot.web;
 
+import jojoidu.boot.springboot.config.auth.dto.SessionUser;
 import jojoidu.boot.springboot.web.dto.PostsResponseDto;
 import jojoidu.boot.springboot.web.service.posts.PostsService;
 import lombok.RequiredArgsConstructor;
@@ -8,11 +9,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     /**
      * TOP画面を表示（記事全部を取得）
@@ -24,6 +28,12 @@ public class IndexController {
     public String index(Model model) {
 
         model.addAttribute("posts", postsService.finAllDesc());
+
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
 
         return "index";
     }
