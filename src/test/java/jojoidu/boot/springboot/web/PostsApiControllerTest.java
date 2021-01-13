9d20f283e81e1cc132c693cfb6a454003d5956f3
twan65 <cx.twan65@gmail.com>
2021-01-13 18:32:57 +0900

    Security追加によるTest NGの対応

--- src/test/java/jojoidu/boot/springboot/web/PostsApiControllerTest.java
+++ src/test/java/jojoidu/boot/springboot/web/PostsApiControllerTest.java
@@ -1,25 +1,32 @@
 package jojoidu.boot.springboot.web;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jojoidu.boot.springboot.domain.posts.Posts;
 import jojoidu.boot.springboot.domain.posts.PostsRepository;
 import jojoidu.boot.springboot.web.dto.PostsSaveRequestDto;
 import jojoidu.boot.springboot.web.dto.PostsUpdateRequestDto;
 import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -34,6 +41,19 @@ public class PostsApiControllerTest {
     @Autowired
     private PostsRepository postsRepository;
 
+    @Autowired
+    private WebApplicationContext context;
+
+    private MockMvc mvc;
+
+    @Before
+    public void setup() {
+        mvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+    }
+
     @After
     public void tearDown() throws Exception {
         postsRepository.deleteAll();
@@ -45,6 +65,7 @@ public class PostsApiControllerTest {
      * @throws Exception
      */
     @Test
+    @WithMockUser(roles = "USER")
     public void savePosts() throws Exception {
         // given
         String title = "title";
@@ -60,12 +81,11 @@ public class PostsApiControllerTest {
         // URLを作成
         String url = "http://localhost:" + port + "/api/v1/posts";
 
-        // RestTemplateを利用してAPIを呼び出す。（URL、パラメータ、戻り値の型）
-        ResponseEntity<Long> responseEntity = restTemplate.postForEntity(url, requestDto, Long.class);
-
-        // 検証
-        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertThat(responseEntity.getBody()).isGreaterThan(0L);
+        // when
+        mvc.perform(post(url)
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .content(new ObjectMapper().writeValueAsString(requestDto)))
+                .andExpect(status().isOk());
 
         // データを取得して検証を行う。
         List<Posts> postsList = postsRepository.findAll();
@@ -80,14 +100,15 @@ public class PostsApiControllerTest {
      * @throws Exception
      */
     @Test
+    @WithMockUser(roles = "USER")
     public void updatePosts() throws Exception {
 
         // 記事登録
         Posts savePosts = postsRepository.save(Posts.builder()
-        .title("title")
-        .content("content")
-        .author("author")
-        .build());
+                .title("title")
+                .content("content")
+                .author("author")
+                .build());
 
         Long updateId = savePosts.getId();
         String updateTitle = "title2";
@@ -101,15 +122,11 @@ public class PostsApiControllerTest {
 
         // URLを作成
         String url = "http://localhost:" + port + "/api/v1/posts/" + updateId;
-
-        HttpEntity<PostsUpdateRequestDto> requestEntity = new HttpEntity<>(requestDto);
-
-        // RestTemplateを利用してAPIを呼び出す。（URL、HTTPメソッド、パラメータ、戻り値の型）
-        ResponseEntity<Long> responseEntity = restTemplate.exchange(url, HttpMethod.PUT, requestEntity, Long.class);
-
-        // 検証
-        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertThat(responseEntity.getBody()).isGreaterThan(0L);
+        // when
+        mvc.perform(put(url)
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .content(new ObjectMapper().writeValueAsString(requestDto)))
+                .andExpect(status().isOk());
 
         // データを取得して検証を行う。
         List<Posts> postsList = postsRepository.findAll();
